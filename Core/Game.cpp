29a13a316a6ab41d2390f866ebb4348ca944c891
msolwold <msolwold35@gmail.com>
2019-11-15 20:37:43 -0800

    Implemented Player Turn

--- Core/Game.cpp
+++ Core/Game.cpp
@@ -26,7 +26,8 @@
 
 using namespace std;
 
-Game::Game(){
+Game::Game()
+{
 
     board = new Square[10];
     player = new Player();
@@ -35,38 +36,42 @@ Game::Game(){
     num_ants = 0;
 }
 
-int Game::run(){
+int Game::run()
+{
 
-    cout << "------ Starting Game ------" << endl << endl;
+    cout << "------ Starting Game ------" << endl
+         << endl;
     create_board();
 
     //core - Do while to force game to run once
     // ? Move to own func?
-    do {
+    do
+    {
 
         spawn_bee();
-        
+
         player_turn();
 
-        ant_attack();
+        ant_attack(); // TODO
         bee_attack();
-
 
     } while (isActive());
 
     // if player is alive return 1, is player dies return 0
-    if (player->isAlive()) return 1;
-    else return 0;
-
+    if (player->isAlive())
+        return 1;
+    else
+        return 0;
 }
-
 
 // * Helpers
 
-void Game::create_board(){
+void Game::create_board()
+{
 
-    for (int i = 0; i < 10; i++){
-        
+    for (int i = 0; i < 10; i++)
+    {
+
         board[i].isBodyguard = false;
         board[i].isAnt = false;
         board[i].isNinja = false;
@@ -82,8 +87,8 @@ void Game::create_board(){
  * Returns false if queen is dead or if there are no bees on the board
  * 
  */
-bool Game::isActive(){
-
+bool Game::isActive()
+{
     return player->isAlive() && num_bees > 0;
 }
 
@@ -92,12 +97,13 @@ bool Game::isActive(){
  * Function that creates templates for each ant type
  * Used for easy printing and ant creation
  * 
- */ 
-void Game::create_ant_templates(){
-    
+ */
+void Game::create_ant_templates()
+{
+
     // Modular for new ants added to the game
     num_ant_types = 8;
-    ant_list = new Ant*[num_ant_types];
+    ant_list = new Ant *[num_ant_types];
 
     ant_list[0] = new Ant_Harvester();
     ant_list[1] = new Ant_Fire();
@@ -109,22 +115,175 @@ void Game::create_ant_templates(){
     ant_list[7] = new Ant_Bodyguard();
 }
 
-void Game::player_turn(){
+void Game::player_turn()
+{
+
+    int player_ant_selection;
+    int player_location_selection;
+
+    while (true)
+    {
+
+        player_ant_selection = get_player_ant_choice() - 1;
+        player_location_selection = get_player_location_choice(player_ant_selection) - 1;
+
+        if (player_location_selection != -1)
+            break;
+    }
+
+    cout << "You've chosen to place a " << ant_list[player_ant_selection]->get_ant_type()
+         << " ant at position " << player_location_selection + 1;
+
+    place_ant(player_location_selection, create_ant(player_ant_selection));
+}
+
+int Game::get_player_ant_choice()
+{
 
     print_ant_list();
+    int selection;
+
+    while (true)
+    {
+
+        cout << "Enter the value of the ant you would like to place: ";
+        cin >> selection;
+
+        if (selection < 1 || selection > num_ant_types)
+            cout << "Your choice was out of the range of valid choices!" << endl
+                 << endl;
+
+        else if (ant_list[selection - 1]->get_ant_cost() > player->get_num_resources())
+            cout << "That ant is too expensive!" << endl
+                 << endl;
+
+        else
+            break;
+    }
+
+    cout << "You selected the " << ant_list[selection - 1]->get_ant_type() << " ant" << endl
+         << endl;
+
+    return selection;
+}
+
+int Game::get_player_location_choice(int ant)
+{
+
+    int selection;
+    int error_choice;
+
+    while (true)
+    {
+
+        cout << "Enter the value of the square that you would like to place your ant in: ";
+        cin >> selection;
+
+        if (selection < 1 || selection > 10)
+            cout << "The value entered was out of bounds!" << endl
+                 << endl;
+
+        else if (can_place_ant(selection, ant_list[ant]))
+            break;
+
+        while (true)
+        {
+
+            cout << endl;
+            cout << "Would you like to try a different position or choose a new Ant?" << endl;
+            cout << "Enter 0 to try again or 1 to choose a new Ant: ";
+
+            cin >> error_choice;
+
+            if (error_choice == 0)
+                break;
+            if (error_choice == 1)
+                return -1;
+
+            cout << "Invalid Entry!" << endl
+                 << endl;
+        }
+    }
+
+    return selection;
+}
+
+Ant *Game::create_ant(int ant_index)
+{
+    return ant_list[ant_index]->clone;
+}
+
+/**
+ * 
+ * Returns true if the ant can be placed at the given location
+ * 
+ * @param int selection index on the board
+ * @param Ant * ant being placed
+ */
+bool Game::can_place_ant(int selection, Ant *a)
+{
+
+    if (a->get_ant_type() == "Bodyguard" && board[selection].isBodyguard)
+    {
+
+        cout << "You cannot place another Bodyguard in this position!" << endl;
+        return false;
+    }
+
+    else if (board[selection].isAnt)
+    {
+
+        cout << "There is already an ant at this position!" << endl;
+        return false;
+    }
+
+    return true;
+}
+
+void Game::place_ant(int index, Ant *a)
+{
+
+    board[index].ants.push_back(a);
+
+    if (a->get_ant_type() == "Bodyguard")
+    {
+        board[index].isBodyguard = true;
+        board[index].isBlocked = true;
+    }
+
+    else if (a->get_ant_type() == "Ninja")
+    {
+        board[index].isNinja = true;
+        board[index].isAnt = true;
+    }
+
+    else
+    {
+        board[index].isAnt = true;
+        board[index].isBlocked = true;
+    }
+
+    num_ants++;
+    board[index].num_ants++;
 }
 
 // * Print Statements
 
-void print_board(){
-
+void Game::print_board()
+{
 }
 
-void print_ant_list(){
+void Game::print_ant_list()
+{
 
-    cout << "Please select an ant to place:\n" << endl; 
+    cout << "Please select an ant to place:\n"
+         << endl;
 
-    for (int i = 0; i < num_ant_types; i++){
-        cout << "(" << i << "). " << ant_list->
+    for (int i = 0; i < num_ant_types; i++)
+    {
+        cout << "(" << i << "). " << ant_list[i]->get_ant_type() << ":" << endl;
+        cout << "   Cost: " << ant_list[i]->get_ant_cost() << endl;
+        cout << "   Armor: " << ant_list[i]->get_ant_armor() << endl;
+        cout << "   Action: " << ant_list[i]->get_ant_action() << endl;
     }
 }
--- Core/Game.h
+++ Core/Game.h
@@ -10,86 +10,97 @@
 #include <iostream>
 #include <vector>
 
-#include "Player.h"
 #include "../Ants/Ant.h"
 #include "Bee.h"
+#include "Player.h"
 
+#include "../Ants/Ant_Harvester.h"
+#include "../Ants/Ant_Fire.h"
+#include "../Ants/Ant_Ninja.h"
+#include "../Ants/Ant_Wall.h"
+#include "../Ants/Ant_Bodyguard.h"
+#include "../Ants/Throwers/Ant_Thrower.h"
+#include "../Ants/Throwers/Ant_ShortThrower.h"
+#include "../Ants/Throwers/Ant_LongThrower.h"
 
-class Game {
+class Game
+{
 
-    public:
+public:
+    // * Constructors
 
-        // * Constructors
+    Game();
 
-        Game();
+    // ? Dont need to create copy constructors
 
-        // ? Dont need to create copy constructors
+    // * Destructor
 
-        // * Destructor
+    ~Game();
 
-        ~Game();
+    // * Accessors
 
-        // * Accessors
+    int run();
 
-        int run();
+    // * Mutators
 
-        // * Mutators
+private:
+    // * Struct to place in each square of the board
+    struct Square
+    {
 
+        std::vector<Ant *> ants;
+        std::vector<Bee *> bees;
 
-    private:
+        bool isBodyguard;
+        bool isAnt;
+        bool isNinja;
+        bool isBlocked;
 
-        // * Struct to place in each square of the board
-        struct Square {
-
-            std::vector<Ant *> ants;
-            std::vector<Bee *> bees;
-
-            bool isBodyguard;
-            bool isAnt;
-            bool isNinja;
-            bool isBlocked;
-
-            int num_ants;
-            int num_bees;
-
-        };
-
-        Player * player;
-        Square * board;
-
-        Ant ** ant_list;                    // templates of each ant type
-        int num_ant_types;                  // If new ants are added
-
-        int num_bees;
         int num_ants;
+        int num_bees;
+    };
 
-        // * Helpers
+    Player *player;
+    Square *board;
 
-        void create_board();
-        bool isActive();                    // return queen status and num_bees > 0
+    Ant **ant_list;    // templates of each ant type
+    int num_ant_types; // If new ants are added
 
-        void create_ant_templates();        // fills ant_list array
+    int num_bees;
+    int num_ants;
 
-        void check_square(int index);       // Print Space Information (toString for the struct)
-        void refresh_squares();             // Run every turn to check the status of each square and reset bee attack flag
+    // * Helpers
 
-        void spawn_bee();
-        void bee_attack();
-        void move_bee(int curr, int dest, Bee * b);
-        void kill_bee(int index, Bee * b);
+    void create_board();
+    bool isActive(); // return queen status and num_bees > 0
 
-        void player_turn();
-        bool place_ant(int index, Ant * a); // Try to place the ant, return false if unable
-        void ant_attack();
-        void get_action(Ant * a);
-        void kill_ant(int index, Ant * a); 
+    void create_ant_templates(); // fills ant_list array
 
-        // * Print Statements
+    void check_square(int index); // Print Space Information (toString for the struct)
+    void refresh_squares();       // Run every turn to check the status of each square and reset bee attack flag
 
-        void print_board();
-        void print_ant_list();
-        void print_place_ant();
+    void spawn_bee();
+    void bee_attack();
+    void move_bee(int curr, int dest, Bee *b);
+    void kill_bee(int index, Bee *b);
 
+    void player_turn();
+    int get_player_ant_choice();
+    int get_player_location_choice(int ant);
+
+    Ant *create_ant(int ant_index);
+    bool can_place_ant(int index, Ant *a); // Try to place the ant, return false if unable
+    void place_ant(int index, Ant *a);     // Place the ant on the board
+
+    void ant_attack();
+    void get_action(Ant *a);
+    void kill_ant(int index, Ant *a);
+
+    // * Print Statements
+
+    void print_board();
+    void print_ant_list();
+    void print_place_ant();
 };
 
 #endif
