f55e3ac5ab8cae91fe5fbd4e593c1f40c061aaae
msolwold <msolwold35@gmail.com>
2019-11-17 23:27:00 -0800

    main

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,7 @@
  * Main
  */
 
-#include "Game.h"
+#include "Core/Game.h"
 
 #include <string>
 #include <iostream>
@@ -11,5 +11,13 @@ using namespace std;
 
 int main()
 {
+    Game * g = new Game();
+
+    int result = g->run();
+
+    if (result == 1) cout << endl << "You Won!!" << endl;
+    if (result == 0) cout << endl << "You Lost??" << endl;
+
+    delete g;
     return 1;
 }
